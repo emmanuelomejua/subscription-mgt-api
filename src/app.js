8f47393d9f0724483139c5963f727523d5fbc27f
Emmanuel Omejua <omejuaifeanyi2@mail.com>
2025-12-11 13:44:53 +0100

    implement app routes

--- src/app.js
+++ src/app.js
@@ -2,11 +2,29 @@ import express from 'express';
 
 import { PORT } from './config/env.js';
 
+
+// **ROUTES IMPORT** /
+import authRouter from './routes/auth.route.js';
+import userRouter from './routes/user.route.js';
+import subscriptionRouter from './routes/subscription.route.js';
+
 const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({limit: '50mb'}))
 
 app.get('/', (req, res) => {
     res.send('Server is active!!!')
 })
+
+
+/****
+ * Routes Uses
+ */
+app.use('api/v1/auth', authRouter);
+app.use('api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter)
 
 
 app.listen(PORT, () => {
---
+++ src/routes/subscription.route.js
@@ -0,0 +1,6 @@
+import { Router } from "express";
+
+const subscriptionRouter = Router();
+
+
+export default subscriptionRouter;
