ea4d21fe264b4a25ef86b1d84fb018410f3b3ae8
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2025-12-19 14:08:46 +0100

    implement auth controllers, get user and get all users endpoints

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,6 +1,12 @@
 import { Router } from "express";
 
+import { getAllUsers, getUser } from "../controllers/user.controller.js";
+
 const userRouter = Router();
+
+userRouter.get('/', getAllUsers)
+
+userRouter.get('/:id', getUser)
 
 
 export default userRouter;
